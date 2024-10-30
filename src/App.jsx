fdcc9c2dd081d68b9e3fe64a9a8b8fa5997779c7
nasrwihtw <Nasr.al-Sayaghi@Student.HTW-Berlin.de>
2024-10-30 23:53:13 +0100

    update app file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
 import About from "./components/About/About"
 import Skills from "./components/Skills/Skills";
-import Projects  from "./components/projects/projects";
+import Projects  from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 
 function App() {
